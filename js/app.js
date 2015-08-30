1e002bd3eb8aa5bb48ad9ff9f7c3b058f592b0c7
ZB <Zoeb2760@gmail.com>
2015-08-30 11:46:47 -0400

    added input list items to list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,67 @@
 $(document).ready(function() { 
-	
-  }); 
+
+  
+ $("#mybutton").on('click',  (function  () {
+  var items = $('input').val(); 
+   
+    $('ul').append('<li>'+items+'</li>');
+ })); 
+	//$('#mybutton')
+//  
+//alert("button was clicked") ; 
+ // $("<ul>").append("<li>add to list</li>");
+  
+
+
+  // $("ul").append("<li> add to list</li>");
+        
+
+        // $("p").append("Appended item"); 
+    });
 
 
 
+//});
 
-
-
-$('ul').on('click', 'li', function(event){
+ 
+	//$('<ul>').append ('<li>  add to list</li>'); 
+//}) ;
+    //var food = $('input').val(); } )
+	//var x = li;
+	//return li;
+		//{ alert('clicked') testing button
   // fires when any LIs are clicked on
   // including LIs that aren't on the page when it is initially loaded
-});
+//});
+	
+  //}); 
+
+
+
+//
+
+
+
+  //var multipleValues = $( "#multiple" ).val() || [];
+
+
+//$('ul').on('click', 'li', function(.append){ .append li
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+//});
+
+
+
+//$('ul').on('click', 'li', function(event){
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+//});
+
+
+
+// $( "body" ).append( $newdiv1, [ newdiv2, existingdiv1 ] );
+
+/*function displayVals() {
+  var singleValues = $( "#single" ).val();
+  var multipleValues = $( "#multiple" ).val() || []; */
+
