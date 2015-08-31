ab5403d7c772cda82aa9ece8384fdf61aaba936b
ZB <Zoeb2760@gmail.com>
2015-08-31 15:11:08 -0400

    Removed li from ul
    removed li from ul to prevent empty first li

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 $(document).ready(function() { 
 
-  
- $('#mybutton').on('click',  (function  () {
+  // adding items to the list
+ $('#mybutton').on('click',  (function  () {/*
+  When mybutton is clicked take new formed var and add to li */
    var items = $('input').val();
    $('ul').append('<li>'+items+'</li>');
    
+// clearing the list
+    $('form').find('input').val(''); // to clear the form
 
-    $('form').find('input').val("");
+    //$('li').prepend (html('&#1003';);
+
+    $('li').prepend("\u2713"); // adding dup checks
+    
+   // to clear enter list
 
 
-   
+   //})
+
+
 
  })); 
+ 
+ $('#clearbutton').on('click', (function() {
+    $('ul').empty();
+ }));
 
 });
 	
